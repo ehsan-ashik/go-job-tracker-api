12b7bcbe2ded2e73fec9a7e9fbf317a7881818c8
Ehsan Ul Haque <16237525+ehsan-ashik@users.noreply.github.com>
2025-01-19 21:09:02 -0500

    CORS added

--- cmd/main.go
+++ cmd/main.go
@@ -7,10 +7,13 @@ import (
 	"github.com/ehsan-ashik/go-job-tracker-api/internal/filestorageservice"
 	"github.com/ehsan-ashik/go-job-tracker-api/router"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	filestorageservice.ServiceClientSharedKey(os.Getenv("AZURE_ACCOUNT_NAME"), os.Getenv("AZURE_ACCESS_KEY"))
 	database.ConnectDB()
